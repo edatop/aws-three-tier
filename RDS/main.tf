92b865d383e8b2032b92c4729411237d3b3b469b
edatop <edatp.hir@gmail.com>
2023-09-20 22:49:10 +0000

    RDS and VPC folders are added

---
+++ RDS/main.tf
@@ -0,0 +1,45 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = false
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "fdbpass" {
+  name  = var.db_name
+  type  = "SecureString"
+  value = random_password.password.result
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated_storage
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  db_name              = var.db_name
+  username             = var.username
+  password             = random_password.password.result
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible  = var.publicly_accessible
+  db_subnet_group_name = "my-db-subnet" # Use the same DB subnet group name as in aws_db_subnet_group
+}
+
+resource "aws_db_subnet_group" "example" {
+  name        = "my-db-subnet" # Use the same DB subnet group name as in aws_db_instance
+  description = "Custom DB subnet group description"
+
+  subnet_ids = ["subnet-0704b2588f21e4111", "subnet-0d872c9f3271f4f9a"] # Replace with your actual subnet IDs
+}
+
+resource "aws_security_group" "rds_security_group" {
+  name        = "my-rds-security-group"
+  description = "Security group for RDS instance"
+
+  # Define ingress rules to allow incoming MySQL traffic from specific sources
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # You can restrict this to specific IP ranges
+  }
+}
---
+++ RDS/variable.tf
@@ -0,0 +1,70 @@
+variable "region" {
+  description = "Please provide a region name"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Please provide a tag for resource"
+  type        = map(any)
+  default     = {}
+}
+
+variable "allocated_storage" {
+  type        = number
+  default     = 20
+  description = "Please provide a value"
+}
+
+variable "engine" {
+  type        = string
+  default     = ""
+  description = "Please provide a value"
+}
+
+variable "engine_version" {
+  type        = string
+  default     = ""
+  description = "Please provide a value"
+}
+
+variable "instance_class" {
+  type        = string
+  default     = ""
+  description = "Please provide a value"
+}
+
+variable "db_name" {
+  type        = string
+  default     = ""
+  description = "Please provide a value"
+}
+
+variable "username" {
+  type        = string
+  default     = ""
+  description = "Please provide a value"
+}
+
+variable "password" {
+  type        = string
+  default     = ""
+  description = "Please provide a value"
+}
+
+variable "publicly_accessible" {
+  type        = bool
+  default     = true
+  description = "Please provide a value"
+}
+
+variable "db_subnet_group_name" {
+  type        = string
+  description = "Name of the DB subnet group"
+  default     = ""
+}
+
+variable "db_instance_identifier" {
+  description = "The identifier for the RDS instance"
+  type        = string
+}
